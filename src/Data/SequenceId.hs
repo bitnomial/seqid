d87a28b93e5ba8ca5b165e0d411a91e9c87d7953
Matthew Wraith <matt@bitnomial.com>
2018-10-26 11:34:00 -0500

    Use consistent variables

--- src/Data/SequenceId.hs
+++ src/Data/SequenceId.hs
@@ -56,9 +56,9 @@ data SequenceIdErrorType
 
 -- | If the current sequence ID is greater than 1 more than the last
 -- sequence ID then the appropriate error is returned.
-checkSeqIdM :: (Integral a, Monad m)
-            => a -- ^ Current sequence ID
-            -> SequenceIdT a m (Maybe (SequenceIdError a))
+checkSeqIdM :: (Integral s, Monad m)
+            => s -- ^ Current sequence ID
+            -> SequenceIdT s m (Maybe (SequenceIdError s))
 checkSeqIdM currSeq = do
     lastSeq <- get
     put $ max lastSeq currSeq
@@ -67,32 +67,32 @@ checkSeqIdM currSeq = do
 
 -- | If the difference between the sequence IDs is not 1 then the
 -- appropriate error is returned.
-checkSeqId :: Integral a
-           => a -- ^ Last sequence ID
-           -> a -- ^ Current sequence ID
-           -> Maybe (SequenceIdError a)
+checkSeqId :: Integral s
+           => s -- ^ Last sequence ID
+           -> s -- ^ Current sequence ID
+           -> Maybe (SequenceIdError s)
 checkSeqId lastSeq currSeq
     | delta lastSeq currSeq > 1 = Just $ SequenceIdError SequenceIdDropped    lastSeq currSeq
     | delta lastSeq currSeq < 1 = Just $ SequenceIdError SequenceIdDuplicated lastSeq currSeq
     | otherwise                 = Nothing
 
 
-delta :: Integral a => a -> a -> Int
+delta :: Integral s => s -> s -> Int
 delta lastSeq currSeq = fromIntegral currSeq - fromIntegral lastSeq
 
 
 -- | Update to the next sequense ID
-incrementSeqIdM :: (Monad m, Integral a) => SequenceIdT a m a -- ^ Next sequence ID
+incrementSeqIdM :: (Monad m, Integral s) => SequenceIdT s m s -- ^ Next sequence ID
 incrementSeqIdM = modify' incrementSeqId >> get
 
 
 -- | Increment to the next sequense ID
-incrementSeqId :: Integral a
-               => a -- ^ Last sequence ID
-               -> a -- ^ Next sequence ID
+incrementSeqId :: Integral s
+               => s -- ^ Last sequence ID
+               -> s -- ^ Next sequence ID
 incrementSeqId = (+1)
 
 
 -- | Last seen sequense ID
-lastSeqIdM :: Monad m => SequenceIdT a m a -- ^ Last sequence ID
+lastSeqIdM :: Monad m => SequenceIdT s m s -- ^ Last sequence ID
 lastSeqIdM = get
